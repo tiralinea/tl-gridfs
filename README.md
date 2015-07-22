11395cb6355f2f46db55d2f0639a1bf1fc286826
Santiago G. Marín <santiago@finaldevstudio.com>
2015-07-22 16:43:48 -0300

    Completed testing and code optimization

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
 # fi-seed-component-gridfs
 Fi Seed's GridFS component
+
+## Usage
+
+### Use on fi-seed
+
+```js
+var auth = component('gridfs');
+```
+
+### Use on Express app
+
+```js
+var auth = require('fi-seed-component-gridfs');
+```
+
+### Initialization
+You must call it with your Express's app instance, to attach the routes, and a configuration object:
+
+```js
+auth(app, config);
+```
+
+### Configuration
+The configuration object must have an authorizer function and a route array. The `debug` parameter is optional but recommended.
+
+**IMPORTANT:** All routes are allowed by default!
+
+- **debug**:
+  - This option can be a `String` to use the [debug](https://github.com/visionmedia/debug) module or `true` to use `console.log`.
+
+- **authorizer**:
+  - This is required and must be a `function`. This function is run on each request and should return the value that will be evaluated against the `allows` parameter value inside each route definition. The authorizer value will be attached to `req.session.authorized`.
+
+- **routes**:
+  - An `Array` with the routes to authorize:
+    - **method**: A `String` or an `Array` of HTTP request methods to filter. If no method is specified it defaults to 'ALL';
+    - **route**: A `String` or an `Array` of strings to filter.
+    - **allows**: A `String` or an `Array` of authorization values to filter:
+
+```js
+{
+  debug: 'app:auth',
+
+  authorizer: function (req) {
+    if (req.session.user) {
+      return req.session.user.admin && 'admin' || 'user';
+    }
+
+    return false;
+  },
+
+  routes: [{
+    method: 'GET',
+    route: '/api/users',
+    allows: 'admin'
+  }, {
+    method: ['POST', 'PUT', 'DELETE'],
+    route: ['/api/users', '/api/stuff'],
+    allows: 'admin'
+  }, {
+    method: ['POST', 'PUT', 'DELETE'],
+    route: '/api/content',
+    allows: ['user', 'admin']
+  }]
+}
+```
---
+++ test/index.js
@@ -0,0 +1,284 @@
+'use strict';
+
+var MongoClient = require('mongodb').MongoClient;
+var expect = require('chai').expect;
+var Stream = require('stream');
+var crypto = require('crypto');
+var mongo = require('mongodb');
+var figrid = require('../');
+var path = require('path');
+var fs = require('fs');
+var os = require('os');
+
+var paths = {
+  fixtures: path.join(__dirname, 'fixtures')
+};
+
+var files = {
+  buffer: new Buffer(crypto.randomBytes(64).toString('hex')),
+  image: path.join(paths.fixtures, 'image.png'),
+  text: path.join(paths.fixtures, 'text.txt')
+};
+
+var mimetypes = {
+  txt: 'text/plain',
+  png: 'images/png',
+  bin: 'binary/octet-stream'
+};
+
+var saved = [];
+
+var server, database;
+
+function rint(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+describe('FiGrid', function () {
+  var id;
+
+  before(function (done) {
+    var url = 'mongodb://localhost:27017/fi-grid-tests';
+
+    MongoClient.connect(url, function (err, db) {
+      if (err) {
+        return done(err);
+      }
+
+      database = db;
+
+      done();
+    });
+  });
+
+  describe('object', function () {
+    it('should be and object', function () {
+      expect(figrid).to.be.an.instanceof(Object);
+    });
+
+    it('should initialize the module and store the parameters', function () {
+      figrid.init(database, mongo);
+
+      expect(figrid.mongo).to.equal(mongo);
+      expect(figrid.db).to.equal(database);
+    });
+
+    it('should return the same instance as before', function () {
+      var grid = require('../');
+
+      expect(grid).to.equal(figrid);
+      expect(grid.mongo).to.equal(mongo);
+      expect(grid.db).to.equal(database);
+    });
+  });
+
+  describe('write', function () {
+    it('should be a function', function () {
+      expect(figrid.write).to.be.a('function');
+    });
+
+    it('should write a text file from a path string without options', function (done) {
+      figrid.write(files.text, function (err, fsfile) {
+        if (err) {
+          return done(err);
+        }
+
+        expect(err).to.be.null;
+        expect(fsfile).to.be.an('object');
+        expect(fsfile._id).to.be.an.instanceof(mongo.ObjectID);
+
+        saved.push(fsfile._id);
+
+        done();
+      });
+    });
+
+    it('should write an image file from a read stream without options', function (done) {
+      var stream = fs.createReadStream(files.image);
+
+      figrid.write(stream, function (err, fsfile) {
+        if (err) {
+          return done(err);
+        }
+
+        expect(err).to.be.null;
+        expect(fsfile).to.be.an('object');
+        expect(fsfile._id).to.be.an.instanceof(mongo.ObjectID);
+
+        saved.push(fsfile._id);
+
+        done();
+      });
+    });
+
+    it('should write data from a buffer without options', function (done) {
+      figrid.write(files.buffer, function (err, fsfile) {
+        if (err) {
+          return done(err);
+        }
+
+        expect(err).to.be.null;
+        expect(fsfile).to.be.an('object');
+        expect(fsfile._id).to.be.an.instanceof(mongo.ObjectID);
+
+        saved.push(fsfile._id);
+
+        done();
+      });
+    });
+
+    it('should write a text file from a path string with options', function (done) {
+      var filename = path.basename(files.text);
+      var options = {
+        filename: filename,
+        content_type: mimetypes.txt
+      };
+
+      figrid.write(files.text, options, function (err, fsfile) {
+        if (err) {
+          return done(err);
+        }
+
+        expect(err).to.be.null;
+        expect(fsfile).to.be.an('object');
+        expect(fsfile._id).to.be.an.instanceof(mongo.ObjectID);
+        expect(fsfile.filename).to.equal(filename);
+        expect(fsfile.contentType).to.equal(mimetypes.txt);
+
+        saved.push(fsfile._id);
+
+        done();
+      });
+    });
+
+    it('should write an image file from a read stream with options', function (done) {
+      var stream = fs.createReadStream(files.image);
+      var filename = path.basename(files.image);
+      var options = {
+        filename: filename,
+        content_type: mimetypes.png
+      };
+
+      figrid.write(stream, options, function (err, fsfile) {
+        if (err) {
+          return done(err);
+        }
+
+        expect(err).to.be.null;
+        expect(fsfile).to.be.an('object');
+        expect(fsfile._id).to.be.an.instanceof(mongo.ObjectID);
+        expect(fsfile.filename).to.equal(filename);
+        expect(fsfile.contentType).to.equal(mimetypes.png);
+
+        saved.push(fsfile._id);
+
+        done();
+      });
+    });
+
+    it('should write data from a buffer with options', function (done) {
+      var filename = 'buffer.bin';
+      var options = {
+        filename: filename,
+        content_type: mimetypes.bin
+      };
+
+      figrid.write(files.buffer, options, function (err, fsfile) {
+        if (err) {
+          return done(err);
+        }
+
+        expect(err).to.be.null;
+        expect(fsfile).to.be.an('object');
+        expect(fsfile._id).to.be.an.instanceof(mongo.ObjectID);
+        expect(fsfile.filename).to.equal(filename);
+        expect(fsfile.contentType).to.equal(mimetypes.bin);
+
+        saved.push(fsfile._id);
+
+        done();
+      });
+    });
+  });
+
+  describe('read', function () {
+    it('should be a function', function () {
+      expect(figrid.read).to.be.a('function');
+    });
+
+    it('should read a file from GridFS by its ID', function (done) {
+      var id = saved[rint(0, saved.length - 1)];
+
+      figrid.read(id, function (err, fsfile, read) {
+        if (err) {
+          return done(err);
+        }
+
+        expect(err).to.be.null;
+        expect(fsfile).to.be.an('object');
+        expect(fsfile._id.equals(id)).to.be.true;
+        expect(read).to.be.an.instanceof(Stream.Readable);
+
+        done();
+      });
+    });
+
+    it('should read a file from GridFS by its file name', function (done) {
+      var filename = path.basename(files.text);
+
+      figrid.read(filename, function (err, fsfile, read) {
+        if (err) {
+          return done(err);
+        }
+
+        expect(err).to.be.null;
+        expect(fsfile).to.be.an('object');
+        expect(fsfile.filename).to.equal(filename);
+        expect(read).to.be.an.instanceof(Stream.Readable);
+
+        done();
+      });
+    });
+  });
+
+  describe('remove', function () {
+    it('should be a function', function () {
+      expect(figrid.remove).to.be.a('function');
+    });
+
+    it('should remove a file from GridFS by its ID', function (done) {
+      var id = saved[rint(0, saved.length - 1)];
+
+      figrid.remove(id, function (err) {
+        if (err) {
+          return done(err);
+        }
+
+        expect(err).to.be.null;
+
+        done();
+      });
+    });
+
+    it('should remove a file from GridFS by its file name', function (done) {
+      var filename = path.basename(files.text);
+
+      figrid.remove(filename, function (err, fsfile, read) {
+        if (err) {
+          return done(err);
+        }
+
+        expect(err).to.be.null;
+
+        done();
+      });
+    });
+  });
+
+  after(function (done) {
+    database.dropDatabase(function () {
+      database.close();
+      done();
+    });
+  });
+});
